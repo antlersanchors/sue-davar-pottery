f96bd4d2c598b7b06908db1c00d278bedc5489ac
Antlers and Anchors <michaelowen.liston@gmail.com>
2012-12-29 14:23:23 -0500

    Added webfonts back in. Enqueued in custom.php.

--- wp-content/themes/roots/lib/custom.php
+++ wp-content/themes/roots/lib/custom.php
@@ -1,3 +1,11 @@
 <?php
 
 // Custom functions
+
+// Add me some Google Web Fonts action
+function load_fonts() {
+        wp_register_style('googleFonts','http://fonts.googleapis.com/css?family=Pacifico|Raleway:100,500,600');
+        wp_enqueue_style( 'googleFonts');
+    }
+
+add_action('wp_print_styles', 'load_fonts'); ?>
