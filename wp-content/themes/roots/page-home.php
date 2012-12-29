c51b7b64f2505ada91430cf41f05dbfdb10dfe1e
Antlers and Anchors <michaelowen.liston@gmail.com>
2012-12-29 12:26:32 -0500

    Realized home-header.php was redundant, so commented out call for it in page-home.php and deleted it.
    home-header.php just generates a header, which we’re not using on the
    home page.

--- wp-content/themes/roots/page-home.php
+++ wp-content/themes/roots/page-home.php
@@ -1,2 +1,5 @@
-<?php get_template_part('templates/home', 'header'); ?>
+<?php 
+/* Removed call for header, as no title necessary on Home Page 
+get_template_part('templates/home', 'header'); */ 
+?>
 <?php get_template_part('templates/content', 'home'); ?>
